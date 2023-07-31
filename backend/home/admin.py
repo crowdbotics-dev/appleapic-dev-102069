5777b6f0fd9e965c84f175a3def3142edfd5a860
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-31 11:09:00 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ttrtdfsyauhdjfdgkjmjngkjsdngjknsdgkjnsjdngkjdngkjnsdkjgnkjdfsngkjdsf
+admin.site.register(Ttrtdfsyauhdjfdgkjmjngkjsdngjknsdgkjnsjdngkjdngkjnsdkjgnkjdfsngkjdsf)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Ttrtdfsyauhdjfdgkjmjngkjsdngjknsdgkjnsjdngkjdngkjnsdkjgnkjdfsngkjdsf(models.Model):
+    'Generated Model'
+    testijoklfndkjngkjdfngndkjgndfkjngkjdfngkjndfg = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="ttrtdfsyauhdjfdgkjmjngkjsdngjknsdgkjnsjdngkjdngkjnsdkjgnkjdfsngkjdsf_testijoklfndkjngkjdfngndkjgndfkjngkjdfngkjndfg",)
