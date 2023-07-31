d24dcea09e1f8b16e1e8653a7479c35666803876
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 07:47:22 +0000

    Update store.

--- store/appleapicdevAPI/api.js
+++ store/appleapicdevAPI/api.js
@@ -1,85 +1,65 @@
-import axios from "axios";
+import axios from "axios"
 const appleapicdevAPI = axios.create({
   baseURL: "https://appleapic-dev-102069.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_login_create(payload) {
+  return appleapicdevAPI.post(`/api/v1/login/`, payload)
+}
+function api_v1_signup_create(payload) {
+  return appleapicdevAPI.post(`/api/v1/signup/`, payload)
+}
+function rest_auth_user_retrieve(payload) {
+  return appleapicdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return appleapicdevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return appleapicdevAPI.patch(`/rest-auth/user/`, payload)
+}
 function api_docs_schema_retrieve(payload) {
   return appleapicdevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
-function api_v1_login_create(payload) {
-  return appleapicdevAPI.post(`/api/v1/login/`, payload);
-}
-
-function api_v1_signup_create(payload) {
-  return appleapicdevAPI.post(`/api/v1/signup/`, payload);
-}
-
 function rest_auth_login_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/login/`, payload);
+  return appleapicdevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return appleapicdevAPI.get(`/rest-auth/logout/`);
+  return appleapicdevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/logout/`);
+  return appleapicdevAPI.post(`/rest-auth/logout/`)
 }
-
-function rest_auth_password_change_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/password/change/`, payload);
-}
-
-function rest_auth_password_reset_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/password/reset/`, payload);
-}
-
-function rest_auth_password_reset_confirm_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
-}
-
 function rest_auth_registration_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/registration/`, payload);
+  return appleapicdevAPI.post(`/rest-auth/registration/`, payload)
 }
-
+function rest_auth_password_reset_create(payload) {
+  return appleapicdevAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_change_create(payload) {
+  return appleapicdevAPI.post(`/rest-auth/password/change/`, payload)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return appleapicdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
 function rest_auth_registration_verify_email_create(payload) {
-  return appleapicdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return appleapicdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
-function rest_auth_user_retrieve(payload) {
-  return appleapicdevAPI.get(`/rest-auth/user/`);
-}
-
-function rest_auth_user_update(payload) {
-  return appleapicdevAPI.put(`/rest-auth/user/`, payload);
-}
-
-function rest_auth_user_partial_update(payload) {
-  return appleapicdevAPI.patch(`/rest-auth/user/`, payload);
-}
-
 export const apiService = {
-  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update,
+  api_docs_schema_retrieve,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
   rest_auth_registration_create,
-  rest_auth_registration_verify_email_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update
-};
+  rest_auth_password_reset_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_verify_email_create
+}
